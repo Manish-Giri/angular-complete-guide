4bcf6959f427490ed509485ffd784764fc382f7e
manish-giri <manish.giri.me@gmail.com>
2020-06-06 20:59:53 -0400

    Emit custom events from Cockpit, listen to them in AppComponent

--- 5-components-databinding/src/app/app.component.ts
+++ 5-components-databinding/src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
   serverElements = [{type: 'server', name: 'Test Server', content: 'This a dummy server'}];
 
 
+  onServerAdded(serverData: {name: string, content: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.name,
+      content: serverData.content
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {name: string, content: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.name,
+      content: blueprintData.content
+    });
+  }
 }
